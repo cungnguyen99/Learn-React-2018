51ea721543ab826ba5f272551185e1f6cf924389
Cung Nguyen <cungnv2912@gmail.com>
2020-02-28 19:57:25 -0800

    React 12 learn about state part 1

---
+++ traffic-light/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TrafficLight from './components/TrafficLight'
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <TrafficLight/>
+      </div>
+    );
+
+  }
+}
+
+export default App;
---
+++ traffic-light/src/components/TrafficLight.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import classNames from 'classnames'
+import './TrafficLight.css'
+const RED=0;
+const ORANGE=1;
+const GREEN=2;
+class TrafficLight extends Component {
+  constructor(){
+      super();
+      /**
+       * gọi constructor của lớp cha(lớp Components) và thêm một thuộc tính nữa là currentColor 
+       * gtri ban đầu là RED, học trong bài 13 JS nâng cao kế thừa trong class, ta sẽ viết như sau
+       * this.currentColor=RED; nhưng ta phải dùng state. state là biến để lưu giá trị nội tại của
+       * một component. mà currentColor là một biến thay đổi liên tục không giữ nguyên một giá trị
+       * cố định nên ta phải xét cho nó là kiểu state và có thể viết là this.state.currentColor=RED;
+       * nhưng ta có cách viết tương tự
+       */
+      this.state={
+          currentColor: RED
+      }
+
+      setInterval(()=>{
+          /**
+           * Khi sử dụng state rồi mà muốn thay đổi giá trị của state đó ta không viết là
+           * this.state.currentColor= gì đó mà ta phải dùng setState và truyền vào thuộc tính muốn thay đổi
+           * Được phép lấy ra giá trị của state là this.state.currentColor nhưng không được phép
+           * thay đổi trục tiếp giá trị của state bằng cách this.state.currenColor= giá trị nào đó
+           * mà phải dùng setState.
+           * TRONG MỘT REACT COMPONENT THÌ HÀM render() ĐƯỢC TỰ ĐỘNG GỌI KHI DATA THAY ĐỔI CHÍNH VÌ THẾ
+           * MÀ TA KHÔNG CẦN GỌI NÓ TRONG HÀM getNextColor() NHƯNG NÓ VẪN THỰC HIỆN NHỮNG GÌ ĐƯỢC VIẾT
+           * TRONG HÀM render()
+           */
+          this.setState({ 
+            currentColor: this.getNextColor(this.state.currentColor)
+          })
+      },1000)
+  }
+
+  getNextColor(color){
+    switch(color){
+        case RED: 
+            return ORANGE;
+        case ORANGE:
+            return GREEN;
+        case GREEN:
+            return RED;
+        default: 
+            return RED;
+    }
+  }
+  render(){
+    const {currentColor}=this.state;
+    return (
+      <div className="TrafficLight">
+          {/*sử dụng modul classNames truyền vào tên các class và điều kiện của nó để sd class đó nếu có */}
+        <div className={classNames('bulb','red',{
+            //currentColor===RED tương đương this.state.currentColor===RED
+            'active': currentColor===RED
+        })}/>
+        <div className={classNames('bulb','orange',{
+            'active': currentColor===ORANGE
+        })}/>
+        <div className={classNames('bulb','green',{
+            'active': currentColor===GREEN
+        })}/>
+      </div>
+    );
+
+  }
+}
+
+export default TrafficLight;
