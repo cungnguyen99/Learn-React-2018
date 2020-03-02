3aeb68f97fd89401458ecb56b7e9645c1c9a3341
Cung Nguyen <cungnv2912@gmail.com>
2020-03-01 20:01:50 -0800

    learn about Bài 13 - Handling events: not done

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 class App extends Component {
-  constructor() {
-    super();
-    this.todoList = [
-      { title: 'Go to shopping',time:'5h', isComplete: true },
-      { title: 'Go to school',time:'7h' },
-      { title: 'Play game', time:'9h30' }
-    ]
+  /**
+   * Phải thêm biến state vào rồi bind this ở dưới để hàm onItemClick biết biến this đấy là của App
+   */
+  constructor(state) {
+    super(state);
+    this.state={
+      todoList : [
+        { title: 'Go to shopping',time:'5h', isComplete: true },
+        { title: 'Go to school',time:'7h' },
+        { title: 'Play game', time:'9h30' }
+      ]
+    }
+    this.onItemClick=this.onItemClick.bind(this)
+  }
+
+  onItemClick(index){
+    this.setState({
+      todoList: this.state.todoList[index].isComplete=!this.state.todoList[index].isComplete
+    })
+    console.log(this.state.todoList)
   }
   render() {
+    const {todoList}=this.state
     /**
      * Render ra với điều kiện cho trước( với cách bình thường): nếu k có sản phẩm thì render ra chữ Nothing here
      * còn nếu có sp thì render ra các sản phẩm
@@ -18,10 +32,11 @@ class App extends Component {
     return (
       <div className="App">
         {
-          this.todoList.length>0&&this.todoList.map((item, index) => <TodoItem key={index} item={item} />)
+          todoList.length>0&&todoList.map((item, index)=> 
+           <TodoItem key={index} item={item} onclick={()=>this.onItemClick(index)} />)
         }
         {
-          this.todoList.length===0&&'Nothing here'
+          todoList.length===0&&'Nothing here'
         }
       </div>
     );
--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './TodoItem.css'
 var classNames = require('classnames');
 /**
- * BÀI LIÊN QUAN 6,7,8,9,10,11
+ * BÀI LIÊN QUAN 6,7,8,9,10,11,13
  */
 class TodoItem extends Component {
   render() {
     //giống với việc viết item=this.props.item học trong bài js nâng cao khi 2 biến trùng tên
-    const { item } = this.props
+    const { item, onclick } = this.props
     let className = "todoItem"
 
     //sử dụng cách bình thường để thêm class
@@ -20,7 +20,7 @@ class TodoItem extends Component {
       'todoItem-time': item.isComplete
     })
     return (
-      <div className={className}>
+      <div onClick={onclick} className={className}>
         <p>{item.title} <span className={todoTime}>{item.time}</span></p>
       </div>
     );
