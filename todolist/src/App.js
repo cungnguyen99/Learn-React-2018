4d51a5df3faf167e546029b2f34f2ba1a1810011
Cung Nguyen <cungnv2912@gmail.com>
2020-03-06 19:06:43 -0800

    React 17 finish app todoList part 3: not done

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import tick from './img/tick.svg'
 class App extends Component {
   /**
    * Phải thêm biến state vào rồi bind this ở dưới để hàm onItemClick biết biến this đấy là của App
@@ -8,13 +9,20 @@ class App extends Component {
   constructor(state) {
     super(state);
     this.state={
+      newItem:'',
       todoList : [
         { title: 'Go to shopping',time:'5h', isComplete: true },
         { title: 'Go to school',time:'7h' },
         { title: 'Play game', time:'9h30' }
       ]
     }
+    /**
+     * Không có dòng dưới cũng được vì onItemClick ở trong nó cũng đã trả về một func khác(arrow func)
+     * nên không cần bind theo this.
+     */
     this.onItemClick=this.onItemClick.bind(this)
+    this.onKeyUp=this.onKeyUp.bind(this)
+    this.onChange=this.onChange.bind(this)
   }
 
   onItemClick(item){
@@ -48,14 +56,51 @@ class App extends Component {
      }
 
   }
+
+  //sự kiện onchange phải đi cùng với thuộc tính value trong input
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    })
+  }
+  onKeyUp(event){
+    let text=event.target.value
+    if(event.keyCode===13){//key của nút enter là 13
+      if(!text){
+        return
+      }
+      text=text.trim();//cắt bỏ dấu cách ở đầu và cuối
+  
+      if(!text) {
+        return
+      }
+  
+      this.setState({
+        todoList:[
+          {title: text, isComplete:false},
+          ...this.state.todoList
+        ]
+      })
+    }
+  }
   render() {
-    const {todoList}=this.state
+    const {todoList,newItem}=this.state
     /**
      * Render ra với điều kiện cho trước( với cách bình thường): nếu k có sản phẩm thì render ra chữ Nothing here
      * còn nếu có sp thì render ra các sản phẩm
      */
     return (
       <div className="App">
+        <div className="Header">
+          <img src={tick} width={32} height={32}/>
+          <input 
+            type="text"
+            placeholder='Add a new item'
+            onKeyUp={this.onKeyUp}
+            value={newItem}
+            onChange={this.onChange}
+           />
+        </div>
         {
           todoList.length>0&&todoList.map((item, index)=> 
            <TodoItem key={index} item={item} onclick={this.onItemClick(item)} />)
